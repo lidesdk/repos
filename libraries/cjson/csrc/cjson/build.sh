a63f9874f0168eb842eb9271e92f5767bff717b0
dcanoh <thdkano@gmail.com>
2017-02-25 23:25:17 -0500

    Updated cjson windows x86 library
    Uploaded sources for luacjson-luapower

---
+++ libraries/cjson/csrc/cjson/build.sh
@@ -0,0 +1,5 @@
+${X}gcc -c -O2 $C strbuf.c lua_cjson.c fpconv.c -I../lua-headers \
+	-Wall -pedantic -DDISABLE_INVALID_NUMBERS
+${X}gcc *.o -shared -o ../../bin/$P/clib/$D $L
+${X}ar rcs ../../bin/$P/$A *.o
+rm *.o
